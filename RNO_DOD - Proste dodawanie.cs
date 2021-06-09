ed69ecee61fffb8444589cfc896b8d8f7768bbc8
petardaOFF <officialpetarda@gmail.com>
2021-06-09 18:56:31 +0200

    Add files via upload

---
+++ RNO_DOD - Proste dodawanie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace rno_dod_spoj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                Console.WriteLine(AdditionMethod(int.Parse(Console.ReadLine()), Console.ReadLine()));
+            }
+        }
+
+
+
+        static int AdditionMethod(int howMuch, string w)
+        {
+            string[] whichOne = w.Split(' ');
+            int result=0;
+
+
+            for (int i = 0; i < whichOne.Length && i < howMuch; i++)
+            {
+                result += int.Parse(whichOne[i]);  
+            }
+            
+            
+           
+
+
+
+          
+            
+            return result;
+        }
+    }
+}
