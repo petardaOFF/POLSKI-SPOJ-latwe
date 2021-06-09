7e879cfc1d49317ab76d02e5c53b29cecafb21f1
petardaOFF <officialpetarda@gmail.com>
2021-06-09 18:58:45 +0200

    Add files via upload

---
+++ BFN1 - Zabawne Dodawanie Piotrusia.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace BFN1_spoj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                Console.WriteLine(Actions(int.Parse(Console.ReadLine())));
+            }
+               
+        }
+        static string Actions(int x)
+        {
+            int counter=0;
+            int result = x;
+            string textResult = "";
+
+
+            do
+            {
+                if (result == Reverse(result.ToString()))
+                {
+                    textResult = result + " " + counter.ToString();
+                    counter = 0;
+                    return textResult;
+                }
+                else
+                {
+
+                    
+                        result = result + Reverse(result.ToString());
+                        counter++;
+                    textResult = result + " " + counter.ToString();
+
+
+
+
+                }
+               
+            } while (result !=Reverse(result.ToString()));
+               
+            
+          
+
+            return textResult;
+            
+            
+        }
+      static  int Reverse(string str)
+        {
+            char[] charArray = str.ToCharArray();
+            Array.Reverse(charArray);
+
+            int d = int.Parse(charArray);
+            
+            return d ;
+        }
+    }
+}
