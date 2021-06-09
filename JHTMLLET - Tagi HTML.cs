e11a36240d9c61c566188e189c095e6318f7dedd
petardaOFF <officialpetarda@gmail.com>
2021-06-09 17:48:04 +0200

    Add files via upload

---
+++ JHTMLLET - Tagi HTML.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Numerics;
+
+
+namespace JHTMLLET___Tagi_HTML
+{
+    class Program
+    {
+
+        
+       
+        static void Main(string[] args)
+        {
+
+
+          string text = "";
+            
+
+
+
+            while((text = Console.ReadLine()) != null)
+            {
+                string result = "";
+
+
+                for (int i = 0; i < text.Length; )
+                {
+                   
+                    if (text[i] == '<')
+                   {
+                       
+                        while(text[i] != '>')
+                        {
+                            result += char.ToUpper(text[i]);
+                            i++;
+
+
+                           
+                        }
+                   }
+                    else
+                    {
+                        result += text[i];
+                        i++;
+                    }
+                }
+                
+
+                Console.WriteLine(result);
+                
+             
+            }
+         
+        }
+
+
+    }
+       
+}
+  
+
+
+
+
+
+
+
+
