c277cd640884bd87ef2d31b0fef8dde0f8152038
petardaOFF <officialpetarda@gmail.com>
2021-06-09 19:52:09 +0200

    Add files via upload

---
+++ PP0504B - StringMerge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PP0504B___StringMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] words = Console.ReadLine().Split(' ');
+
+                Console.WriteLine(StringCreator(words[0], words[1]));
+            }
+        }
+        static string StringCreator(string a, string b)
+        {
+            string result = "";
+            int lenght = a.Length < b.Length ? a.Length : b.Length;
+
+            for (int i = 0; i < lenght; i++)
+            {
+                result += a[i];
+                result += b[i];
+
+            }
+
+            return result;
+        }
+    }
+}
