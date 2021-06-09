7e0073de5fe7ed72fbe1917d5152009995363f5b
petardaOFF <officialpetarda@gmail.com>
2021-06-09 19:54:44 +0200

    Add files via upload

---
+++ PP0504D - Reprezentacja liczb typu float.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace bytes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i <t; i++)
+            {
+                string text = ToBytes(float.Parse(Console.ReadLine()));
+                string result = AdvandcedReverse(text);
+                Console.WriteLine(result.ToLower());
+
+
+
+            }
+
+
+        }
+        private static string AdvandcedReverse(string t)
+        {
+            string result = "";
+            string[] txt = t.Split("-");
+
+            for(int i = txt.Length-1; i >=0; i--)
+            {
+                
+                if(txt[i][0]=='0')
+                {
+                    result += txt[i][1];
+                }
+                else
+                {
+                    result += txt[i];
+                }
+                if(i>0)
+                {
+                    result += " ";
+                }
+                
+                
+               
+            }
+
+
+
+            return result;
+        }
+
+        static string ToBytes(float v) => BitConverter.ToString(BitConverter.GetBytes(v));
+
+
+
+
+
+
+
+
+    }
+}
