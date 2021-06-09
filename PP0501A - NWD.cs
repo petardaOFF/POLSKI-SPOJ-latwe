5ce1c7242542a2c378b3d211861f5c8cf6c03673
petardaOFF <officialpetarda@gmail.com>
2021-06-09 19:20:52 +0200

    Add files via upload

---
+++ PP0501A - NWD.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace PP0501A_spoj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < t; i++)
+            {
+                Console.WriteLine(nwd(Console.ReadLine()));
+                
+            }
+        }
+        static int nwd(string str)
+        {
+
+            string[] numbers = str.Split(' ');
+            int a = int.Parse(numbers[0]);
+            int b = int.Parse(numbers[1]);
+
+            while(a != b)
+            {
+                if(a>b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    b -= a;
+                }
+            }
+            
+            
+            
+            
+            
+            return a;
+        }
+    }
+}
