03d3e3032fe158cc450dcf02effa3cba13f4af9f
petardaOFF <officialpetarda@gmail.com>
2021-06-09 21:33:14 +0200

    Add files via upload

---
+++ SUMA - Suma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace no
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            
+            int result=0;
+            while((a=Console.ReadLine()) !=null)
+            {
+                Console.WriteLine(result += int.Parse(a));
+            }
+            
+        }
+    }
+}
