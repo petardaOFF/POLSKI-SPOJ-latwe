ad654ec16b6ef904e30c6aa50aac70ea7be0617f
petardaOFF <officialpetarda@gmail.com>
2021-06-09 18:25:56 +0200

    Add files via upload

---
+++ PA05_POT - Czy umiesz potęgować.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+            int D = int.Parse(Console.ReadLine());
+            for (int i = 0; i < D; i++)
+            {
+
+              
+                
+              Console.WriteLine(ResultMethod(Console.ReadLine()));
+
+
+            }
+         
+        }
+
+        static int ResultMethod(string s)
+        {
+            string[] numbers = s.Split(' ');
+            int a = int.Parse(numbers[0])%10;
+            int b = int.Parse(numbers[1]);
+            int wynik = a;
+            if(b%4==0)
+            {
+                b = 4;
+            }
+            else
+            {
+                b %= 4;
+            }
+            for (int i = 1; i < b; i++)
+            {
+                wynik *= a;
+            }
+           
+           
+           
+
+            return wynik%10; 
+        }
+      
+    }
+}
