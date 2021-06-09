3b6806aa268aa35fd7a277622bc2785cf2bba068
petardaOFF <officialpetarda@gmail.com>
2021-06-09 21:31:10 +0200

    Add files via upload

---
+++ EUCGAME - Gra Euklidesa.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			int t = int.Parse(Console.ReadLine());
+            for (int i = 0; i < t; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(' ');
+                int a = int.Parse(tokens[0]);
+                int b = int.Parse(tokens[1]);
+
+               while(a !=b)
+               {
+                    if(a>b)
+                    {
+                        a -= b;
+                    }  
+                    else
+                    {
+                        b -= a;
+                    }
+               }
+                Console.WriteLine(b+a);
+
+
+            }
+				
+		}
+    }
+}
+
+
+
+
