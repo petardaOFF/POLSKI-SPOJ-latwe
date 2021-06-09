2ba2c12a12ab379b038e756d913376d1092f0f50
petardaOFF <officialpetarda@gmail.com>
2021-06-09 21:27:19 +0200

    Add files via upload

---
+++ PTROL - ROL.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ROL_k_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Queue<string> myQueue = new Queue<string>(Console.ReadLine().Split(' '));
+                myQueue.Dequeue();
+                var temp = myQueue.Dequeue();
+                myQueue.Enqueue(temp);
+                foreach (var item in myQueue)
+                {
+                    Console.Write(item + ' ');
+
+
+                }Console.WriteLine();
+                
+            }
+            
+        }
+        
+    }
+}
