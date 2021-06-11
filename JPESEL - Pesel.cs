a3b7ce7a5466bab34147cd81345d7e942547d21b
petardaOFF <officialpetarda@gmail.com>
2021-06-11 11:54:58 +0200

    Add files via upload

---
+++ JPESEL - Pesel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace pesel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+           
+            for (int i = 0; i < t; i++)
+            {
+                Console.WriteLine(PeselCheck(Console.ReadLine()));
+
+            }
+        }
+        private static string PeselCheck(string txt)
+        {
+            int[] product = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1 };
+            int result_a=0;
+            int result_b=0;
+            for (int i = 0; i < 11; i++)
+            {
+
+                result_a = txt[i] * product[i];
+                result_b += result_a;
+            }
+
+
+            if (result_b > 0 && result_b%10==0)
+            {
+               
+                
+                    return "D";
+           }
+            else
+                return "N";
+            
+        }
+    }
+}
---
+++ JSPACE - Spacje.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace JDSSPACE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string t;
+            while ((t = Console.ReadLine()) != null)
+            {
+                string result = "";
+                string[] txt = t.Split(' ', '\t');
+                for (int i = 1; i < txt.Length; i++)
+                {
+                    if(txt[i].Length>0)
+                    {
+                        txt[i] = char.ToUpper(txt[i][0]) + txt[i].Substring(1);
+                    }
+                  
+
+
+                }
+
+                foreach (var item in txt)
+                {
+                    result += item;
+                }
+
+               
+
+                Console.WriteLine(result);
+
+            }
+        }
+        
+
+
+    }
+}
