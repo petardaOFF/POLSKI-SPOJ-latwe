0bc5db4e149cb1fe78216f501d4dd64840bb7857
petardaOFF <officialpetarda@gmail.com>
2021-06-11 11:25:21 +0200

    Add files via upload

---
+++ CALC - Kalkulator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace spoj_calc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] txt;
+            while((txt = Console.ReadLine().Split(' ')) != null)
+            {
+                int a = int.Parse(txt[1]), b = int.Parse(txt[2]), result=0;
+               
+                
+                switch(txt[0])
+                {
+                    case "+":
+                        {
+                            result = a + b;  
+                        }
+                        break;
+                    case "-":
+                        {
+                            result = a - b;
+                        }
+                        break;
+                    case "*":
+                        {
+                            result = a * b;
+                        }
+                        break;
+                    case "/":
+                        {
+                            result = a / b;
+                        }
+                        break;
+                    case "%":
+                        {
+                            result = a % b;
+                        }
+                        break;
+                }
+                Console.WriteLine(result);
+
+
+
+
+            }
+
+
+        }
+    }
+}
---
+++ CALC2 - Kalkulator 2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace spoj_calc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+           
+            string text;
+            int[] cache = new int[10];
+            while ((text = Console.ReadLine()) != null)
+            {
+                string[] txt = text.Split(' ');
+
+
+                int a = int.Parse(txt[1]);
+                int b = int.Parse(txt[2]);
+
+                switch (txt[0])
+                {
+                    case "+":
+                        {
+                            Console.WriteLine(cache[a] + cache[b]);
+
+                        }
+                        break;
+
+                    case "-":
+                        {
+                            Console.WriteLine(cache[a] - cache[b]);
+                        }
+                        break;
+
+                    case "*":
+                        {
+                            Console.WriteLine(cache[a] * cache[b]);
+                        }
+                        break;
+
+                    case "/":
+                        {
+                            Console.WriteLine(cache[a] / cache[b]);
+                        }
+                        break;
+
+                    case "%":
+                        {
+                            Console.WriteLine(cache[a] % cache[b]);
+                        }
+                        break;
+                    case "z":
+                        {
+                            cache[a] = b;
+
+                        }
+                        break;
+
+
+                }
+                
+                  
+                
+             
+
+
+
+
+            }
+        }
+    }
+}
